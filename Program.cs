3036645ecda36d0abba26c777fe630058913f45d
Drincann <1019933576@qq.com>
2022-05-02 23:44:16 +0800

    chore: 更新示例

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
 ﻿var app = new Angie.Application();
+var router = new Angie.Router();
 
-app.get("/hello", (ctx) => {
-    ctx.setStatus(200).resHTML("<h1>Hello World</h1>");
+app.use(
+
+  router.get("/hello/:name", (ctx) => {
+    ctx.setStatus(200).resHTML($"<h1>Hello {ctx.getRouteParam("name")}</h1>");
+  }).post("/hello", (ctx) => {
+    Console.WriteLine(ctx.req.body);
+    ctx.resJson(new {
+      message = "Hello World"
+    });
+  })
+
+).use((ctx, next) => {
+  ctx.setStatus(404).resHTML("<h1> 404 Not Found </h1>");
 }).listen(80);
